b51acf6f299979a18a895337201687c9b88a712d
docampolcr <docampolcr@docampolcr>
2016-06-29 21:49:52 -0600

    Change-Id: Ib1be5ad8fb462866a4f0d247fd0f5ebbae8cb3dc

--- Project_KAE/data/config.php
+++ Project_KAE/data/config.php
@@ -21,8 +21,8 @@ $config = array(
 		"paths" => array(
 				"resources" => "/path/to/resources",
 				"images" => array(
-						"content" => $_SERVER["DOCUMENT_ROOT"] . "/images/content",
-						"layout" => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
+						"content" => $_SERVER["DOCUMENT_ROOT"] . "/img/content",
+						"layout" => $_SERVER["DOCUMENT_ROOT"] . "/img/layout"
 				)
 		)
 );
